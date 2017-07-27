3affab0eb0524c1d748175893348ad82dfd661ae
ddunbar88 <derekdunbar@mac.com>
2017-07-27 04:56:32 +0000

    Carrierwave / AWS / controller cleanup

--- app/controllers/instructor/courses_controller.rb
+++ app/controllers/instructor/courses_controller.rb
@@ -1,6 +1,6 @@
 class Instructor::CoursesController < ApplicationController
   before_action :authenticate_user!
-  before_action :require_authorized_for_current_course, only [:show]
+  before_action :require_authorized_for_current_course, only: [:show]
 
   def new
     @course = Course.new
@@ -8,34 +8,30 @@ class Instructor::CoursesController < ApplicationController
 
   def create
     @course = current_user.courses.create(course_params)
-     if @course.valid?
+    if @course.valid?
       redirect_to instructor_course_path(@course)
     else
       render :new, status: :unprocessable_entity
     end
-end
+  end
 
   def show
-   @course = Course.find(params[:id])
   end
 
-def show
-end
+  private
 
-private
-
-def require_authorized_for_current_course
-  if current_course.user != current_user
-    render plain: "Unauthroized", status: :unauthroized
+  def require_authorized_for_current_course
+    if current_course.user != current_user
+      render plain: "Unauthorized", status: :unauthorized
+    end
   end
-end
 
-helper_method :current_course
-def current_course
-   @current_course ||= Course.find(params[:id])
- end
+  helper_method :current_course
+  def current_course
+    @current_course ||= Course.find(params[:id])
+  end
 
- def course_params
-   params.require(:course).permit(:title, :description, :cost)
- end
+  def course_params
+    params.require(:course).permit(:title, :description, :cost, :image)
+  end
 end
