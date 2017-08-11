4c91319445b449446f096f36ff039e4297d2cb61
ddunbar88 <derekdunbar@mac.com>
2017-08-11 04:34:17 +0000

    Modal for lesson

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   resource :dashboard, only: [:show]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#index'
   resources :courses, only: [:index, :show] do
     resources :enrollments, only: :create
@@ -9,12 +8,11 @@ Rails.application.routes.draw do
   resources :lessons, only: [:show]
   namespace :instructor do
     resources :lessons, only: [:update]
-    resources :sections, only: [] do
-       resources :lessons, only: [:new, :create]
-     end
-       resources :courses, only: [:new, :create, :show] do
-        resources :sections, only: [:create]
+    resources :sections, only: [:update] do
+      resources :lessons, only: [:create]
+    end
+    resources :courses, only: [:new, :create, :show] do
+      resources :sections, only: [:create]
     end
   end
 end
-
